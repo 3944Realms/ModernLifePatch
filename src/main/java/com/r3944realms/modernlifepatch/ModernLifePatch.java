a7a5d885322ffe88d1b248f15f0a447cf63fd517
3944Realms <f256198830@hotmail.com>
2024-09-25 15:56:54 +0800

    CREATE

---
+++ src/main/java/com/r3944realms/modernlifepatch/ModernLifePatch.java
@@ -0,0 +1,10 @@
+package com.r3944realms.modernlifepatch;
+
+import net.minecraftforge.fml.common.Mod;
+
+import static com.r3944realms.modernlifepatch.ModernLifePatch.MOD_ID;
+
+@Mod(value = MOD_ID)
+public class ModernLifePatch {
+    public static final String MOD_ID = "modernlifepatch";
+}
