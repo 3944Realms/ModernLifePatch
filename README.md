cbc8846b80fc2847838c656e0de19aac491056be
3944Realms <f256198830@hotmail.com>
2024-09-27 23:17:43 +0800

    Update

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Modern Life Patch
-Just a patch
+A patch was made due to too many flaws in the original author's graphic design.
+因为原作者的美工槽点太多，而做的一个补丁。
--- src/main/java/com/r3944realms/modernlifepatch/mixin/block/bathroom/MixinShowerHead.java
+++ src/main/java/com/r3944realms/modernlifepatch/mixin/block/bathroom/MixinShowerHead.java
@@ -16,8 +16,6 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import java.util.stream.Stream;
-
 @Mixin(ShowerHeadBlock.class)
 public abstract class MixinShowerHead extends AbstractWallBlock implements EntityBlock {
     @Unique
